35d418ecd5a385eb78c47ab88f298fb105960618
Sofia Panchaud <s.panchaud.89@gmail.com>
2025-10-04 12:53:35 -0400

    create GET path and DB queries for books api

--- server/controllers/books.controller.ts
+++ server/controllers/books.controller.ts
@@ -1,9 +1,12 @@
 import { Request, Response } from 'express';
 
-import {getAllBooks} from '../models/queries/books/getBooks.ts';
+import {getAllBooks, getOneBookById} from '../models/queries/books/getBooks.ts';
 
+/*
+  Test Path, to be converted to a keyword search path 
+  that searches the external API/DB for titles and authors.
+*/ 
 export const getBooks = async (req: Request, res: Response) => {
-  console.log("Getting All Books...");
   getAllBooks()
   .then(books => {
     console.log('inside getBooks', books);
@@ -11,9 +14,17 @@ export const getBooks = async (req: Request, res: Response) => {
   }).catch(error => console.error('Error in Books controller', error));
 };
 
+/*
+  Get book details by ID
+*/
 export const getBookById = (req: Request, res: Response) => {
   const { id } = req.params;
-  res.json({ message: `Book with id ${id}` });
+  getOneBookById(id)
+  .then(book => {
+    console.log("One book", book);
+    res.json(book);
+  })
+  .catch(error => console.error('Error in controller', error));
 };
 
 export const createBook = (req: Request, res: Response) => {
--- server/models/queries/books/getBooks.ts
+++ server/models/queries/books/getBooks.ts
@@ -9,3 +9,15 @@ export const getAllBooks = () => {
       return results;
     }).catch(error => console.error("Error fetching books", error));
 }
+
+export const getOneBookById = (id:string) => {
+  const values = [id];
+  const query = `
+    SELECT * FROM books
+    WHERE id=$1;
+  `
+
+  return AppDataSource.query(query, values)
+    .then(results => results[0])
+    .catch(error => console.error(`Error fetching book with id ${id}`, error))
+}
