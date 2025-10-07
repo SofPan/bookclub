a9a7f187f47260e38a2afd2b949eec78da303dec
Sofia Panchaud <s.panchaud.89@gmail.com>
2025-10-07 13:15:56 -0400

    add delete route to user routes index

--- server/routes/users.routes.ts
+++ server/routes/users.routes.ts
@@ -1,6 +1,7 @@
 import {Router} from 'express';
 
 import {
+  deleteUser,
   editUser,
   getUser,
   postUser,
@@ -11,6 +12,6 @@ const router = Router();
 router.get('/:id', getUser);
 router.post('/', postUser);
 router.put('/:id', editUser);
-
+router.delete('/:id', deleteUser);
 
 export default router;
